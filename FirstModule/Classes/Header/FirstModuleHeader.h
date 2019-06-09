77b1564b87972cde1e9fa3a5b8529017961f390d
dafiger <dafiger2000@qq.com>
2019-06-09 11:55:22 +0800

    update to FirstModule tag:0.1.0

---
+++ FirstModule/Classes/Header/FirstModuleHeader.h
@@ -0,0 +1,15 @@
+//
+//  FirstModuleHeader.h
+//  FirstModule
+//
+//  Created by dafiger on 2014.
+//  Copyright © 2014 dafiger. All rights reserved.
+//
+
+#ifndef FirstModuleHeader_h
+#define FirstModuleHeader_h
+
+#import "MediatorManager.h"
+#import "FirstProtocol.h"
+
+#endif /* FirstModuleHeader_h */
---
+++ FirstModule/Classes/ModuleManager/FirstEntry.h
@@ -0,0 +1,20 @@
+//
+//  FirstEntry.h
+//  ProtocolDemo
+//
+//  Created by wangpf on 2019/5/23.
+//  Copyright © 2019 wpf. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "FirstProtocol.h"
+
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FirstEntry : NSObject<FirstProtocol>
+
+@end
+
+NS_ASSUME_NONNULL_END
